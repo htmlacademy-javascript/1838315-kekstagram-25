e4f29581f086b7c660b0c71c51b41a888ba15c05
Танюша <solarevatk@inbox.ru>
2022-02-19 21:47:20 +0300

    second-task1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // получение на вход двух чисел,макисмума и минимума, вывод случайного значения
 //формула нахождения числа взята из https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-const getRandomNumber = (min, max) => {
+const getRandomNumber = (min = '1', max = '2') => {
 
   if(min >= 0 && max >= 0) {
 
@@ -14,14 +14,7 @@ const getRandomNumber = (min, max) => {
 // Функция для проверки максимальной длины строки.
 // Будет использоваться для проверки длины введённого комментария, но должна быть универсальна.
 
-const getStringLength = (string, maxLength) => {
-  if(string.length > maxLength) {
-    return true;
-  }
-  else {
-    return false;
-  }
-};
+const getStringLength = (string = 'строка', maxLength = '140') => string.length > maxLength;
 
 getRandomNumber(1,10);
 getStringLength('Привет, я строка, я не более 140 симвлолв', 140);
